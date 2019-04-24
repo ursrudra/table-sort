9cbf8a30d6ed2312fdce05538956c02f3b7d7823
Rudra Kishore <giduturirudrakishore@gmail.com>
2019-04-24 23:36:08 +0530

    name editd row

--- src/components/coin-table/index.jsx
+++ src/components/coin-table/index.jsx
@@ -17,17 +17,31 @@ export default function CoinTable(props) {
       <tbody>
       {
            props.data.map(row => (
-               <tr>
-                   <td>{row.rank}</td>
-                   <td>{row.name}</td>
-                   <td>{row.symbol}</td>
-                   <td>{row.price_usd}</td>
-                   <td>{row.percent_change_1h}</td>
-                   <td>{row.percent_change_24h}</td>
-                   <td>{row.percent_change_7d}</td>
+        <div>       
+        <tr>
+              <td>{row.rank}</td>
+              <td>{row.name}</td>
+              <td>{row.symbol}</td>
+              <td>{row.price_usd}</td>
+              <td>{row.percent_change_1h}</td>
+              <td>{row.percent_change_24h}</td>
+              <td>{row.percent_change_7d}</td>
 
 
-               </tr>
+          </tr>
+          
+          <tr>
+          <td><input type='text' defaultValue ={row.rank}/></td>
+          <td><input type='text' defaultValue ={row.name}/></td>
+          <td><input type='text' defaultValue ={row.symbol}/></td>
+          <td><input type='text' defaultValue ={row.price_usd}/></td>
+          <td><input type='text' defaultValue ={row.percent_change_1h}/></td>
+          <td><input type='text' defaultValue ={row.percent_change_24h}/></td>
+          <td><input type='text' defaultValue ={row.percent_change_7d}/></td>
+
+
+      </tr>
+      </div>
            ))
       }
 
